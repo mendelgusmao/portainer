3a40d16d2f9c9ad0e0dba000135d09792031c8a8
MendelGusmao <mendelsongusmao@gmail.com>
2025-10-21 14:15:28 -0300

    implement stack restart with docker compose force-recreate

--- app/portainer/rest/stack.js
+++ app/portainer/rest/stack.js
@@ -17,7 +17,7 @@ function StackFactory($resource, API_ENDPOINT_STACKS) {
       remove: { method: 'DELETE', params: { id: '@id', external: '@external', endpointId: '@endpointId' } },
       getStackFile: { method: 'GET', params: { id: '@id', action: 'file' } },
       migrate: { method: 'POST', params: { id: '@id', action: 'migrate', endpointId: '@endpointId' }, ignoreLoadingBar: true },
-      start: { method: 'POST', params: { id: '@id', action: 'start', endpointId: '@endpointId' } },
+      start: { method: 'POST', params: { id: '@id', action: 'start', endpointId: '@endpointId', forceRecreate: '@forceRecreate' } },
       stop: { method: 'POST', params: { id: '@id', action: 'stop', endpointId: '@endpointId' } },
       updateGit: { method: 'PUT', params: { id: '@id', action: 'git', subaction: 'redeploy' } },
       updateGitStackSettings: { method: 'POST', params: { id: '@id', action: 'git' }, ignoreLoadingBar: true },
