f79d255b32021a033eb838bf311d3563f0e5641c
MendelGusmao <mendelsongusmao@gmail.com>
2026-01-27 20:35:30 -0300

    Use same style as in containers list

--- app/react/docker/stacks/ListView/StacksDatatable/TableActions.tsx
+++ app/react/docker/stacks/ListView/StacksDatatable/TableActions.tsx
@@ -1,11 +1,11 @@
 import { Authorized } from '@/react/hooks/useUser';
 
-import { AddButton } from '@@/buttons';
+import { AddButton, ButtonGroup } from '@@/buttons';
 import { DeleteButton } from '@@/buttons/DeleteButton';
-import { RestartButton } from '@@/buttons/RestartButton';
-import { StopButton } from '@@/buttons/StopButton';
+import { Button } from '@@/buttons/Button';
 
 import { DecoratedStack } from './types';
+import { RefreshCw, RotateCw, Square } from 'lucide-react';
 
 export function TableActions({
   selectedItems,
@@ -19,31 +19,41 @@ export function TableActions({
   onStop: (items: Array<DecoratedStack>) => void;
 }) {
   return (
-    <div className="flex items-center gap-2">
-      <Authorized authorizations="PortainerStackUpdate">
-        <RestartButton
-          disabled={selectedItems.length === 0}
-          onClick={() => onRestart(selectedItems)}
-          data-cy="stack-restartStackButton"
-        />
-      </Authorized>
+    <div className="flex gap-2">
+      <ButtonGroup>
+        <Authorized authorizations="PortainerStackCreate">
+          <Button
+            color="light"
+            disabled={selectedItems.length === 0}
+            onClick={() => onRestart(selectedItems)}
+            data-cy="stack-restartStackButton"
+            icon={RefreshCw}
+          >
+            Restart
+          </Button>
+        </Authorized>
 
-      <Authorized authorizations="PortainerStackUpdate">
-        <StopButton
-          disabled={selectedItems.length === 0}
-          onClick={() => onStop(selectedItems)}
-          data-cy="stack-stopStackButton"
-        />
-      </Authorized>
+        <Authorized authorizations="PortainerStackUpdate">
+          <Button
+            color="light"
+            disabled={selectedItems.length === 0}
+            onClick={() => onStop(selectedItems)}
+            data-cy="stack-stopStackButton"
+            icon={Square}
+          >
+            Stop
+          </Button>
+        </Authorized>
 
-      <Authorized authorizations="PortainerStackDelete">
-        <DeleteButton
-          disabled={selectedItems.length === 0}
-          onConfirmed={() => onRemove(selectedItems)}
-          confirmMessage="Do you want to remove the selected stack(s)? Associated services will be removed as well."
-          data-cy="stack-removeStackButton"
-        />
-      </Authorized>
+        <Authorized authorizations="PortainerStackDelete">
+          <DeleteButton
+            disabled={selectedItems.length === 0}
+            onConfirmed={() => onRemove(selectedItems)}
+            confirmMessage="Do you want to remove the selected stack(s)? Associated services will be removed as well."
+            data-cy="stack-removeStackButton"
+          />
+        </Authorized>
+      </ButtonGroup>
 
       <Authorized authorizations="PortainerStackCreate">
         <AddButton data-cy="stack-addStackButton" to=".newstack">
